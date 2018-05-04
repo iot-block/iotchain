ba7a7212fba4a6f92c3601dbb682a15869417385
siyan <blazing.siyan@gmail.com>
2018-05-04 11:05:00 +0800

    add M[Boolean] syntax

---
+++ common/src/main/scala/jbok/common/implicits/MonadSyntax.scala
@@ -0,0 +1,22 @@
+package jbok.common.implicits
+
+import cats.implicits._
+import cats.{Functor, Monad}
+
+trait MonadSyntax {
+  implicit final def booleanSyntax[F[_]](fa: F[Boolean]): MonadBooleanOps[F] = new MonadBooleanOps(fa)
+}
+
+final class MonadBooleanOps[F[_]](val a: F[Boolean]) extends AnyVal {
+  def &&(b: => F[Boolean])(implicit M: Monad[F]) = {
+    M.ifM(a)(b, M.pure(false))
+  }
+
+  def ||(b: => F[Boolean])(implicit M: Monad[F]) = {
+    M.ifM(a)(M.pure(true), b)
+  }
+
+  def unary_!(implicit F: Functor[F]) = {
+    a.map(!_)
+  }
+}
---
+++ common/src/main/scala/jbok/common/package.scala
@@ -0,0 +1,5 @@
+package jbok
+
+import jbok.common.implicits.MonadSyntax
+
+package object common extends MonadSyntax
