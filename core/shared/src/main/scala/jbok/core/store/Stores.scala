0bbcc4674f731f6a021219721e0b9ea66f360d5c
siyan <blazing.siyan@gmail.com>
2018-07-13 15:32:11 +0800

    add common model stores

--- core/shared/src/main/scala/jbok/core/store/Stores.scala
+++ core/shared/src/main/scala/jbok/core/store/Stores.scala
@@ -4,8 +4,10 @@ import cats.data.OptionT
 import cats.effect.Sync
 import cats.implicits._
 import jbok.core.models.{BlockBody, BlockHeader, Receipt, _}
-import jbok.persistent.KeyValueDB
+import jbok.crypto.authds.mpt.{MPTrie, Node}
+import jbok.persistent.{KeyValueDB, KeyValueStore}
 import scodec.bits.ByteVector
+import jbok.codec.codecs._
 
 class BlockHeaderStore[F[_]: Sync](db: KeyValueDB[F])
     extends KeyValueStore[F, ByteVector, BlockHeader](Namespaces.BlockHeader, db)
@@ -22,22 +24,31 @@ class BlockNumberHashStore[F[_]: Sync](db: KeyValueDB[F])
 class TransactionLocationStore[F[_]: Sync](db: KeyValueDB[F])
     extends KeyValueStore[F, ByteVector, TransactionLocation](Namespaces.TransactionLocation, db)
 
-class AppStateStore[F[_]: Sync](db: KeyValueDB[F]) extends KeyValueStore[F, String, ByteVector](Namespaces.AppStateNamespace, db) {
+class AppStateStore[F[_]: Sync](db: KeyValueDB[F])
+    extends KeyValueStore[F, String, ByteVector](Namespaces.AppStateNamespace, db) {
   private val BestBlockNumber = "BestBlockNumber"
   private val FastSyncDone = "FastSyncDone"
   private val EstimatedHighestBlock = "EstimatedHighestBlock"
   private val SyncStartingBlock = "SyncStartingBlock"
 
-  def getBestBlockNumber: F[BigInt] = get(BestBlockNumber).flatMap(v => decode[BigInt](v))
+  def getBestBlockNumber: F[BigInt] =
+    for {
+      opt <- getOpt(BestBlockNumber)
+      bn <- opt match {
+        case Some(n) => decode[BigInt](n)
+        case None    => BigInt(0).pure[F]
+      }
+    } yield bn
 
-  def putBestBlockNumber(bestBlockNumber: BigInt): F[Unit] = encode(bestBlockNumber).flatMap(bn => put(BestBlockNumber, bn))
+  def putBestBlockNumber(bestBlockNumber: BigInt): F[Unit] =
+    encode(bestBlockNumber).flatMap(bn => put(BestBlockNumber, bn))
 
   def getFastSyncDone: F[Boolean] = get(FastSyncDone).flatMap(v => decode[Boolean](v))
 
   def putFastSyncDone(b: Boolean = true): F[Unit] = encode(b).flatMap(v => put(FastSyncDone, v))
 
   def getEstimatedHighestBlock: F[BigInt] = {
-    val h= for {
+    val h = for {
       v <- OptionT(getOpt(EstimatedHighestBlock))
       h <- OptionT.liftF(decode[BigInt](v))
     } yield h
@@ -45,12 +56,11 @@ class AppStateStore[F[_]: Sync](db: KeyValueDB[F]) extends KeyValueStore[F, Stri
     h.value.map(_.getOrElse(0))
   }
 
-  def putEstimatedHighestBlock(n: BigInt): F[Unit] = {
+  def putEstimatedHighestBlock(n: BigInt): F[Unit] =
     encode(n).flatMap(v => put(EstimatedHighestBlock, v))
-  }
 
   def getSyncStartingBlock: F[BigInt] = {
-    val h= for {
+    val h = for {
       v <- OptionT(getOpt(SyncStartingBlock))
       h <- OptionT.liftF(decode[BigInt](v))
     } yield h
@@ -58,7 +68,27 @@ class AppStateStore[F[_]: Sync](db: KeyValueDB[F]) extends KeyValueStore[F, Stri
     h.value.map(_.getOrElse(0))
   }
 
-  def putSyncStartingBlock(n: BigInt): F[Unit] = {
+  def putSyncStartingBlock(n: BigInt): F[Unit] =
     encode(n).flatMap(v => put(SyncStartingBlock, v))
-  }
+}
+
+class MPTNodeStore[F[_]: Sync](val mpt: MPTrie[F])
+    extends KeyValueStore[F, Address, Account](Namespaces.NodeNamespace, mpt) {
+
+  def getRootHash: F[ByteVector] = mpt.getRootHash
+
+  def getRoot: F[Node] = mpt.getRoot
+
+  def getNodeByHash(hash: ByteVector): F[Node] = mpt.getNodeByHash(hash)
+
+  def size: F[Int] = mpt.size
+
+  def clear(): F[Unit] = mpt.clear()
+}
+
+object MPTNodeStore {
+  def apply[F[_]: Sync](db: KeyValueDB[F]): F[MPTNodeStore[F]] =
+    for {
+      trie <- MPTrie[F](db)
+    } yield new MPTNodeStore[F](trie)
 }
