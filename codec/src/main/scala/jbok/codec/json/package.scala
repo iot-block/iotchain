173cbf09d8807e2edb88719235a80e05a82ab42b
siyan <blazing.siyan@gmail.com>
2018-07-29 04:04:27 +0800

    update json codec

--- codec/src/main/scala/jbok/codec/json/package.scala
+++ codec/src/main/scala/jbok/codec/json/package.scala
@@ -3,10 +3,12 @@ package jbok.codec
 import io.circe._
 import scodec.bits.ByteVector
 
+import scala.concurrent.duration.Duration
+
 package object json {
   implicit val bytesDecoder: Decoder[ByteVector] = Decoder[String].emap(ByteVector.fromHexDescriptive(_))
 
-  implicit val bytesEncoder: Encoder[ByteVector] = Encoder.instance(bv => Json.fromString(bv.toBase64))
+  implicit val bytesEncoder: Encoder[ByteVector] = Encoder.instance(bv => Json.fromString(bv.toHex))
 
   implicit val bigIntDecoder: Decoder[BigInt] = Decoder[String].map[BigInt](x =>
     if(x.startsWith("0x"))
@@ -14,5 +16,10 @@ package object json {
     else
       BigInt(x)
   )
+
   implicit val bigIntEncoder: Encoder[BigInt] = Encoder[String].contramap[BigInt](_.toString(10))
+
+  implicit val durationEncoder: Encoder[Duration] = Encoder.instance[Duration](d => s"${d.length}${d.unit}".asJson)
+
+  implicit val durationDecoder: Decoder[Duration] = Decoder[String].map(s => Duration.apply(s))
 }
