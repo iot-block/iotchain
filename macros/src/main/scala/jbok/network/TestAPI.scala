c8c36b0147511b0d0dc15822b729b579283db7bb
siyan <blazing.siyan@gmail.com>
2018-07-29 21:42:25 +0800

    update test api

--- macros/src/main/scala/jbok/network/TestAPI.scala
+++ macros/src/main/scala/jbok/network/TestAPI.scala
@@ -1,6 +1,7 @@
 package jbok.network
 
 import cats.effect.Sync
+import fs2.async.mutable.Topic
 import io.circe.generic.JsonCodec
 
 @JsonCodec
@@ -13,10 +14,10 @@ trait TestAPI[F[_]] extends JsonRpcAPI[F] {
 
   def grow(age: Int, name: String): R[Person]
 
-//  val events: Topic[F, Option[String]]
+  val events: Topic[F, Option[String]]
 }
 
-class TestApiImpl[F[_]](implicit F: Sync[F]) extends TestAPI[F] {
+class TestApiImpl[F[_]](val events: Topic[F, Option[String]])(implicit F: Sync[F]) extends TestAPI[F] {
   override def foo: R[Int] = F.pure(Right(42))
 
   override def bar: R[String] = F.pure(Right("oho"))
