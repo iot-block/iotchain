12727d5cad5ebfaae9d0f06ebbde9426ab371a3f
siyan <blazing.siyan@gmail.com>
2018-09-07 14:23:08 +0800

    abstract consensus protocol

---
+++ core/shared/src/main/scala/jbok/core/consensus/Consensus.scala
@@ -0,0 +1,44 @@
+package jbok.core.consensus
+
+import jbok.core.History
+import jbok.core.models._
+
+sealed trait ConsensusResult
+object ConsensusResult {
+  case object ImportToTop               extends ConsensusResult
+  case object Pooled                    extends ConsensusResult
+  case class BlockInvalid(e: Throwable) extends ConsensusResult
+}
+
+abstract class Consensus[F[_]](val history: History[F]) {
+
+  /**
+    * 1. common header validate
+    * 2. specific header validate
+    * 3. validate block and header
+    * 4. validate ommers
+    * 5. execute
+    * 6. post validate
+    */
+  def semanticValidate(parentHeader: BlockHeader, block: Block): F[Unit]
+
+  def calcDifficulty(blockTime: Long, parentHeader: BlockHeader): F[BigInt]
+
+  def calcGasLimit(parentGas: BigInt): BigInt = {
+    val GasLimitBoundDivisor: Int = 1024
+    val gasLimitDifference        = parentGas / GasLimitBoundDivisor
+    parentGas + gasLimitDifference - 1
+  }
+
+  def calcBlockMinerReward(blockNumber: BigInt, ommersCount: Int): F[BigInt]
+
+  def calcOmmerMinerReward(blockNumber: BigInt, ommerNumber: BigInt): F[BigInt]
+
+  def getTimestamp: F[Long]
+
+  def prepareHeader(parent: Block, ommers: List[BlockHeader]): F[BlockHeader]
+
+  def run(parent: Block, current: Block): F[ConsensusResult]
+
+  def mine(block: Block): F[Block]
+}
--- core/shared/src/main/scala/jbok/core/consensus/Protocol.scala
+++
@@ -1,7 +0,0 @@
-package jbok.core.consensus
-
-case class Protocol(
-    name: String, // Official short name of the protocol used during capability negotiation.
-    versions: List[Int], // Supported versions of the eth protocol (first is primary).
-    length: List[Long] // Number of implemented message corresponding to different protocol versions.
-)
