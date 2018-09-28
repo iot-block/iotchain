089c22b6d0f429b6ecb550bbfa9c0a0adac760b0
siyan <blazing.siyan@gmail.com>
2018-09-28 14:48:38 +0800

    make spec a mixin

--- common/src/test/scala/jbok/JbokSpec.scala
+++ common/src/test/scala/jbok/JbokSpec.scala
@@ -1,13 +1,28 @@
 package jbok
 
+import cats.effect.IO
 import org.scalatest.prop.PropertyChecks
 import org.scalatest._
+import org.scalatest.concurrent.{AsyncTimeLimitedTests, TimeLimitedTests}
+import org.scalatest.time.Span
 
-abstract class JbokSpec
-    extends WordSpec
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+trait JbokSpec
+    extends WordSpecLike
     with Matchers
     with PropertyChecks
     with BeforeAndAfterAll
     with BeforeAndAfterEach
+    with TimeLimitedTests {
+  override def timeLimit: Span = 60.seconds
+}
 
-abstract class JbokAsyncSpec extends AsyncWordSpec with Matchers
+trait JbokAsyncSpec extends AsyncWordSpec with Matchers with AsyncTimeLimitedTests {
+  override def timeLimit: Span = 60.seconds
+
+  implicit def futureUnitToFutureAssertion(fu: Future[Unit]): Future[Assertion] = fu.map(_ => Succeeded)
+
+  implicit def futureUnitToFutureAssertion(iou: IO[Unit]): Future[Assertion] = iou.map(_ => Succeeded).unsafeToFuture()
+}
