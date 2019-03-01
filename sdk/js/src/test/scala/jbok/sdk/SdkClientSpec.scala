dd870279f713299bd86d63d7d87e819943e47861
playjokes <panjie1992@gmail.com>
2019-03-01 16:35:41 +0800

    remove build-grammar.sh

--- sdk/js/src/test/scala/jbok/sdk/SdkClientSpec.scala
+++ sdk/js/src/test/scala/jbok/sdk/SdkClientSpec.scala
@@ -11,14 +11,15 @@ class SdkClientSpec extends JbokAsyncSpec {
 
   "SdkClient" should {
     "make http request" ignore {
-      val url = "http://localhost:20002"
-      val data =
-        """{"jsonrpc":"2.0","id":"81ec047e-daa4-4b98-af58-aea736722e66","method":"getAccount","params":["928e878a6eb914e6999f1c88ebfa3cf017eef6e5",{"Latest":{}}]}"""
+      val url    = "http://localhost:20002"
       val client = SdkClient.http(url)
-      client.jsonrpc(data).toFuture.map { result =>
-        println(result)
-        result.length > 0 shouldBe true
-      }(EC)
+      client
+        .jsonrpc("getAccount", """["928e878a6eb914e6999f1c88ebfa3cf017eef6e5",{"Latest":{}}]""")
+        .toFuture
+        .map { result =>
+          println(result)
+          result.length > 0 shouldBe true
+        }(EC)
     }
   }
 }
