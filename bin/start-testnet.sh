3611abcabc06bfe7e1bf5487cc3b72a38f0a09d4
playjokes <panjie1992@gmail.com>
2019-05-30 16:50:05 +0800

    add deploy.md

--- bin/start-testnet.sh
+++ bin/start-testnet.sh
@@ -6,10 +6,10 @@ echo "clean test files"
 rm -rf ~/.jbok/test-*
 
 echo "build testnet"
-app/jvm/target/universal/stage/bin/jbok-app build-testnet "0xa9f26854C08E6A707c9378839C24B5c085F8cE11"
+app/jvm/target/universal/stage/bin/network-builder-main
 
 for i in {0..3}
 do
     echo "start node-${i}"
-    nohup app/jvm/target/universal/stage/bin/jbok-app node ~/.jbok/test-${i}/app.json &>/dev/null &
+    nohup app/jvm/target/universal/stage/bin/app-main ~/.jbok/node-${i}/config.yaml &>/dev/null &
 done
