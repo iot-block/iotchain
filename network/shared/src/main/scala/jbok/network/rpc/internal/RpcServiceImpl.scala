36698c5d6a85e8dff6dad3b325d77a0890f57845
liaojie <leason1989@hotmail.com>
2019-08-08 15:40:12 +0800

    mainnet config

--- network/shared/src/main/scala/jbok/network/rpc/internal/RpcServiceImpl.scala
+++ network/shared/src/main/scala/jbok/network/rpc/internal/RpcServiceImpl.scala
@@ -3,13 +3,17 @@ package jbok.network.rpc.internal
 import cats.effect.Sync
 import cats.implicits._
 import jbok.codec._
+import jbok.common.log.Logger
 import jbok.network.rpc.RpcResponse
 
 final class RpcServiceImpl[F[_], P](implicit F: Sync[F]) {
+  private[this] val log = Logger[F]
+
   def execute[A <: Product, B](payload: P)(call: A => F[B])(implicit decoder: Decoder[A, P], encoder: Encoder[B, P]): F[RpcResponse[P]] =
     decoder.decode(payload) match {
       case Left(err) =>
-        F.pure(RpcResponse.Failure[P](400, s"decode error"))
+        log.info(s"decode error:${err}") >>
+          F.pure(RpcResponse.Failure[P](400, s"decode error"))
 
       case Right(arguments) =>
         call(arguments).map { result =>
