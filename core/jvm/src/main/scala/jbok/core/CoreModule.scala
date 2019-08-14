40e851949e5c6e9f5a992d0869820c6c6414392a
liaojie <leason1989@hotmail.com>
2019-08-15 01:05:54 +0800

    sync fail after node restart

--- core/jvm/src/main/scala/jbok/core/CoreModule.scala
+++ core/jvm/src/main/scala/jbok/core/CoreModule.scala
@@ -15,7 +15,7 @@ import jbok.core.consensus.Consensus
 import jbok.core.consensus.poa.clique.{Clique, CliqueConsensus}
 import jbok.core.keystore.{KeyStore, KeyStorePlatform}
 import jbok.core.ledger.{BlockExecutor, History, HistoryImpl}
-import jbok.core.messages.SignedTransactions
+import jbok.core.messages.{SignedTransactions, Status}
 import jbok.core.mining.BlockMiner
 import jbok.core.models.{Block, ChainId}
 import jbok.core.peer.PeerSelector.PeerSelector
@@ -32,28 +32,36 @@ import jbok.persistent.{KVStore, KVStorePlatform}
 class CoreModule[F[_]: TagK](config: FullConfig)(implicit F: ConcurrentEffect[F], cs: ContextShift[F], T: Timer[F]) extends ModuleDef {
   implicit lazy val acg: AsynchronousChannelGroup = ThreadUtil.acgGlobal
 
-  addImplicit[Bracket[F, Throwable]]
-  addImplicit[ContextShift[F]]
-  addImplicit[Timer[F]]
-  addImplicit[Sync[F]]
-  addImplicit[Concurrent[F]]
-  addImplicit[ConcurrentEffect[F]]
-  addImplicit[AsynchronousChannelGroup]
+  def addImplicits(): Unit ={
+    addImplicit[Bracket[F, Throwable]]
+    addImplicit[ContextShift[F]]
+    addImplicit[Timer[F]]
+    addImplicit[Sync[F]]
+    addImplicit[Concurrent[F]]
+    addImplicit[ConcurrentEffect[F]]
+    addImplicit[AsynchronousChannelGroup]
+  }
+
+  def makeConfigs(): Unit ={
+    make[HistoryConfig].from((config: FullConfig) => config.history)
+    make[TxPoolConfig].from((config: FullConfig) => config.txPool)
+    make[BlockPoolConfig].from((config: FullConfig) => config.blockPool)
+    make[MiningConfig].from((config: FullConfig) => config.mining)
+    make[SyncConfig].from((config: FullConfig) => config.sync)
+    make[PeerConfig].from((config: FullConfig) => config.peer)
+    make[DatabaseConfig].from((config: FullConfig) => config.db)
+    make[ServiceConfig].from((config: FullConfig) => config.service)
+    make[KeyStoreConfig].from((config: FullConfig) => config.keystore)
+    make[SSLConfig].from((config: FullConfig) => config.ssl)
+  }
+
+  addImplicits()
   make[FullConfig].from(config)
   make[ChainId].from((config: FullConfig) => config.genesis.chainId)
 
   LoggerPlatform.initConfig[IO](config.log).unsafeRunSync()
 
-  make[HistoryConfig].from((config: FullConfig) => config.history)
-  make[TxPoolConfig].from((config: FullConfig) => config.txPool)
-  make[BlockPoolConfig].from((config: FullConfig) => config.blockPool)
-  make[MiningConfig].from((config: FullConfig) => config.mining)
-  make[SyncConfig].from((config: FullConfig) => config.sync)
-  make[PeerConfig].from((config: FullConfig) => config.peer)
-  make[DatabaseConfig].from((config: FullConfig) => config.db)
-  make[ServiceConfig].from((config: FullConfig) => config.service)
-  make[KeyStoreConfig].from((config: FullConfig) => config.keystore)
-  make[SSLConfig].from((config: FullConfig) => config.ssl)
+  makeConfigs()
 
   make[KVStore[F]].fromResource(KVStorePlatform.resource[F](config.persist))
   make[Metrics[F]].from(new PrometheusMetrics[F]())
@@ -63,23 +71,38 @@ class CoreModule[F[_]: TagK](config: FullConfig)(implicit F: ConcurrentEffect[F]
   make[Consensus[F]].from[CliqueConsensus[F]]
 
   // peer
-  make[Queue[F, Peer[F], SignedTransactions]].fromEffect(MemoryQueue[F, Peer[F], SignedTransactions])
-  make[Queue[F, PeerSelector[F], SignedTransactions]].fromEffect(MemoryQueue[F, PeerSelector[F], SignedTransactions])
-  make[Queue[F, Peer[F], Block]].fromEffect(MemoryQueue[F, Peer[F], Block])
-  make[Queue[F, PeerSelector[F], Block]].fromEffect(MemoryQueue[F, PeerSelector[F], Block])
-  make[Queue[F, Peer[F], Message[F]]].fromEffect(MemoryQueue[F, Peer[F], Message[F]])
+  def makeQueues(): Unit ={
+    make[Queue[F, Peer[F], SignedTransactions]].fromEffect(MemoryQueue[F, Peer[F], SignedTransactions])
+    make[Queue[F, PeerSelector[F], SignedTransactions]].fromEffect(MemoryQueue[F, PeerSelector[F], SignedTransactions])
+    make[Queue[F, Peer[F], Block]].fromEffect(MemoryQueue[F, Peer[F], Block])
+    make[Queue[F, PeerSelector[F], Block]].fromEffect(MemoryQueue[F, PeerSelector[F], Block])
+    make[Queue[F, Peer[F], Message[F]]].fromEffect(MemoryQueue[F, Peer[F], Message[F]])
+    make[Queue[F, Peer[F], Status]].fromEffect(MemoryQueue[F, Peer[F], Status])
+    make[Queue[F, PeerSelector[F], Status]].fromEffect(MemoryQueue[F, PeerSelector[F], Status])
+  }
+  makeQueues()
 
-  make[Consumer[F, Peer[F], SignedTransactions]].from((queue: Queue[F, Peer[F], SignedTransactions]) => queue)
-  make[Producer[F, Peer[F], SignedTransactions]].from((queue: Queue[F, Peer[F], SignedTransactions]) => queue)
+  def makeCP(): Unit ={
+    make[Consumer[F, Peer[F], SignedTransactions]].from((queue: Queue[F, Peer[F], SignedTransactions]) => queue)
+    make[Producer[F, Peer[F], SignedTransactions]].from((queue: Queue[F, Peer[F], SignedTransactions]) => queue)
 
-  make[Consumer[F, PeerSelector[F], SignedTransactions]].from((queue: Queue[F, PeerSelector[F], SignedTransactions]) => queue)
-  make[Producer[F, PeerSelector[F], SignedTransactions]].from((queue: Queue[F, PeerSelector[F], SignedTransactions]) => queue)
+    make[Consumer[F, PeerSelector[F], SignedTransactions]].from((queue: Queue[F, PeerSelector[F], SignedTransactions]) => queue)
+    make[Producer[F, PeerSelector[F], SignedTransactions]].from((queue: Queue[F, PeerSelector[F], SignedTransactions]) => queue)
 
-  make[Consumer[F, Peer[F], Block]].from((queue: Queue[F, Peer[F], Block]) => queue)
-  make[Producer[F, Peer[F], Block]].from((queue: Queue[F, Peer[F], Block]) => queue)
+    make[Consumer[F, Peer[F], Block]].from((queue: Queue[F, Peer[F], Block]) => queue)
+    make[Producer[F, Peer[F], Block]].from((queue: Queue[F, Peer[F], Block]) => queue)
 
-  make[Consumer[F, PeerSelector[F], Block]].from((queue: Queue[F, PeerSelector[F], Block]) => queue)
-  make[Producer[F, PeerSelector[F], Block]].from((queue: Queue[F, PeerSelector[F], Block]) => queue)
+    make[Consumer[F, PeerSelector[F], Block]].from((queue: Queue[F, PeerSelector[F], Block]) => queue)
+    make[Producer[F, PeerSelector[F], Block]].from((queue: Queue[F, PeerSelector[F], Block]) => queue)
+
+    make[Consumer[F, Peer[F], Status]].from((queue: Queue[F, Peer[F], Status]) => queue)
+    make[Producer[F, Peer[F], Status]].from((queue: Queue[F, Peer[F], Status]) => queue)
+
+    make[Consumer[F, PeerSelector[F], Status]].from((queue: Queue[F, PeerSelector[F], Status]) => queue)
+    make[Producer[F, PeerSelector[F], Status]].from((queue: Queue[F, PeerSelector[F], Status]) => queue)
+  }
+
+  makeCP()
 
   make[Option[SSLContext]].fromEffect(SSLContextHelper[F](config.ssl))
   make[IncomingManager[F]]
